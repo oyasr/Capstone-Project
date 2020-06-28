7624292ab52edefa142f350ef1b068106b2e05d2
Osama Yasser <osamayasserr@gmail.com>
2020-06-28 12:07:01 +0200

    Models and migrations

---
+++ app/models.py
@@ -0,0 +1,52 @@
+from . import db
+
+
+class Category(db.Model):
+    __tablename__ = 'categories'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True)
+    products = db.relationship('Product', backref='category', lazy='dynamic')
+
+    def __repr__(self):
+        return f'<Role: {self.id}, {self.name}>'
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(), unique=True)
+    description = db.Column(db.String())
+    price = db.Column(db.Integer)
+    in_stock = db.Column(db.Boolean, default=True)
+    category_id = db.Column(db.Integer, db.ForeignKey('categories.id'))
+
+    def __repr__(self):
+        return f'<Product: {self.id}, {self.name}, {self.category}>'
+
+    def insert(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self):
+        db.session.commit()
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
+
+    def format(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'category': self.category.name,
+            'description': self.description,
+            'price': self.price,
+            'in_stock': self.in_stock
+        }
