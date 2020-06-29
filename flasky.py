adc9a3abec0b2a73833b47f2bd735c939a300b26
Osama Yasser <osamayasserr@gmail.com>
2020-06-29 10:08:03 +0200

    Heroku sample migrate

--- flasky.py
+++ flasky.py
@@ -2,11 +2,12 @@ import os
 from app import create_app, db
 from flask_script import Manager
 from app.models import Category, Product
-from flask_migrate import Migrate
+from flask_migrate import Migrate, MigrateCommand
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 migrate = Migrate(app, db)
 manager = Manager(app)
+manager.add_command('db', MigrateCommand)
 
 
 @app.shell_context_processor
